b1d23f769a6cfc5a3dd3eed4406e59773babb99b
Shu Yamani <shu.yamani@yappli.co.jp>
2022-05-24 13:12:05 +0900

    feat(nvim): update config of dial.nvim

--- .config/nvim/lua/plugins/dial.lua
+++ .config/nvim/lua/plugins/dial.lua
@@ -2,9 +2,16 @@ local augend = require("dial.augend")
 require("dial.config").augends:register_group({
 	-- default augends used when no group name is specified
 	default = {
-		augend.integer.alias.decimal, -- nonnegative decimal number (0, 1, 2, 3, ...)
-		augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
-		augend.date.alias["%Y/%m/%d"], -- date (2022/02/19, etc.)
+		augend.integer.alias.decimal, -- decimal natural number
+		augend.integer.alias.hex, -- hex natural number
+		augend.integer.alias.octal, -- octal natural number
+		augend.integer.alias.binary, -- binary natural number
+		augend.date.alias["%Y/%m/%d"], -- date in the specific format (0 padding)
+		augend.date.alias["%Y-%m-%d"],
+		augend.date.alias["%m/%d"],
+		augend.date.alias["%H:%M"],
+		augend.constant.alias.ja_weekday,
+		augend.constant.alias.ja_weekday_full,
 		augend.constant.alias.bool, -- boolean value (true <-> false)
 	},
 })
