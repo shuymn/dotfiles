c2f204d9b59e8e23d947a606b34f9f8d1b1ce899
Shu Yamani <shu.yamani@yappli.co.jp>
2023-06-19 15:56:30 +0900

    feat(wezterm): update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,38 +1,48 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 
-return {
-	use_ime = true,
-	font = wezterm.font("UDEV Gothic 35NF"),
-	font_size = 12.0,
-	line_height = 1.2,
-	color_scheme = "OneHalfDark",
-	enable_tab_bar = false,
-	hide_tab_bar_if_only_one_tab = true,
-	adjust_window_size_when_changing_font_size = false,
-	disable_default_key_bindings = true,
-	selection_word_boundary = " \t\n{}[]()\"'`,;:",
-	audible_bell = "Disabled",
-	window_decorations = "RESIZE",
-	window_close_confirmation = "NeverPrompt",
-	window_padding = {
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
-	},
-	keys = {
-		{ key = "q", mods = "SUPER", action = "QuitApplication" },
-		{ key = "w", mods = "SUPER", action = wezterm.action({ CloseCurrentTab = { confirm = false } }) },
-		{ key = "v", mods = "SUPER", action = wezterm.action({ PasteFrom = "Clipboard" }) },
-		{ key = "=", mods = "SUPER", action = "IncreaseFontSize" },
-		{ key = "-", mods = "SUPER", action = "DecreaseFontSize" },
-		{ key = "r", mods = "SUPER", action = "ReloadConfiguration" },
-	},
-	mouse_bindings = {
-		{
-			event = { Up = { streak = 1, button = "Left" } },
-			mods = "SUPER",
-			action = "OpenLinkAtMouseCursor",
-		},
+local config = {}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.use_ime = true
+config.font = wezterm.font("UDEV Gothic 35NF")
+config.font_size = 12.0
+config.line_height = 1.2
+config.color_scheme = "OneHalfDark"
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 5
+config.enable_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.adjust_window_size_when_changing_font_size = false
+config.disable_default_key_bindings = true
+config.selection_word_boundary = " \t\n{}[]()\"'`,;:"
+config.audible_bell = "Disabled"
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+config.keys = {
+	{ key = "q", mods = "SUPER", action = act.QuitApplication },
+	{ key = "w", mods = "SUPER", action = act({ CloseCurrentTab = { confirm = false } }) },
+	{ key = "v", mods = "SUPER", action = act({ PasteFrom = "Clipboard" }) },
+	{ key = "=", mods = "SUPER", action = act.IncreaseFontSize },
+	{ key = "-", mods = "SUPER", action = act.DecreaseFontSize },
+	{ key = "0", mods = "SUPER", action = act.ResetFontSize },
+	{ key = "r", mods = "SUPER", action = act.ReloadConfiguration },
+}
+config.mouse_bindings = {
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "SUPER",
+		action = act.OpenLinkAtMouseCursor,
 	},
 }
+
+return config
