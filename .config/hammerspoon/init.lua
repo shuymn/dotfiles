2fcf5c151da095cddd0cfceaab128aba9a84a19b
shuymn <mail@shuymn.me>
2023-07-13 04:39:15 +0900

    feat(hammerspoon): update

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,33 +1,44 @@
 -- defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
+
 hs.window.animationDuration = 0
+hs.hotkey.setLogLevel(0)
 
 local units = {
 	right50 = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
 	left50 = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
-	top50 = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
-	bot50 = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
 }
 
-hs.hotkey.bind({ "option", "control" }, "right", function()
-	local app = hs.application.get("Vivaldi")
-	if app ~= nil and app:isFrontmost() then
-		hs.eventtap.keyStroke({ "command", "shift" }, "l", 0)
+local right = hs.hotkey.new({ "option", "control" }, "right", function()
+	local app = hs.application.frontmostApplication()
+	if app:name() == "Vivaldi" then
+		hs.eventtap.keyStroke({ "command", "shift" }, "l", 200, app)
 	end
-	hs.window.focusedWindow():move(units.right50, nil, true)
+	app:focusedWindow():move(units.right50, nil, true)
 end)
-hs.hotkey.bind({ "option", "control" }, "left", function()
-	local app = hs.application.get("Vivaldi")
-	if app ~= nil and app:isFrontmost() then
-		hs.eventtap.keyStroke({ "command", "shift" }, "h", 0)
+
+local left = hs.hotkey.new({ "option", "control" }, "left", function()
+	local app = hs.application.frontmostApplication()
+	if app:name() == "Vivaldi" then
+		hs.eventtap.keyStroke({ "command", "shift" }, "h", 200, app)
 	end
-	hs.window.focusedWindow():move(units.left50, nil, true)
+	app:focusedWindow():move(units.left50, nil, true)
 end)
-hs.hotkey.bind({ "option", "control" }, "up", function()
-	hs.window.focusedWindow():move(units.top50, nil, true)
-end)
-hs.hotkey.bind({ "option", "control" }, "down", function()
-	hs.window.focusedWindow():move(units.bot50, nil, true)
-end)
-hs.hotkey.bind({ "option", "control" }, "return", function()
-	hs.window.focusedWindow():maximize()
+
+hs.window.filter
+	.new("Vivaldi")
+	:subscribe(hs.window.filter.windowFocused, function()
+		right:enable()
+		left:enable()
+	end)
+	:subscribe(hs.window.filter.windowUnfocused, function()
+		right:disable()
+		left:disable()
+	end)
+
+hs.hotkey.bind({ "command", "shift" }, "p", function()
+	local app = hs.application.frontmostApplication()
+	if app:name() == "Vivaldi" then
+		hs.eventtap.keyStroke({}, 0x66, 200)
+	end
+	hs.eventtap.keyStroke({ "command", "shift" }, "p", 200, app)
 end)
