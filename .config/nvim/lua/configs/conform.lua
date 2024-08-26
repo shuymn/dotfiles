66aae9990368a7a5150db3124a044dc08c09ba52
shuymn <mail@shuymn.me>
2024-08-27 02:59:46 +0900

    feat(nvim): update

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -1,8 +1,8 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    go = { "gofmt", "gofumpt", "goimports" },
+    bash = { "shfmt", "shellcheck" },
   },
 
   -- format_on_save = {
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,11 +1,10 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    -- event = "BufWritePre", -- uncomment for format on save
     opts = require "configs.conform",
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -13,13 +12,34 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "go",
+        "bash",
+      },
+    },
+  },
+
+  {
+    "phaazon/hop.nvim",
+    branch = "v2",
+    lazy = false,
+    config = function()
+      local hop = require "hop"
+      local directions = require("hop.hint").HintDirection
+      hop.setup()
+
+      vim.keymap.set("", "f", function()
+        hop.hint_char1 { direction = directions.AFTER_CURSOR, current_line_only = true }
+      end, { remap = true })
+      vim.keymap.set("", "F", function()
+        hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = true }
+      end, { remap = true })
+    end,
+  },
 }
