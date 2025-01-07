13e86a7d26911526cfeeeeb15770d53c712fdd13
Shu Yamani <shu.yamani@yappli.co.jp>
2025-01-07 13:58:57 +0900

    feat(nvim): update base46 cache path and add new plugins for kitty scrollback and telescope

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = "BufWritePre", -- uncomment for format on save
+    -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
 
@@ -42,4 +42,32 @@ return {
       end, { remap = true })
     end,
   },
+
+  {
+    'mikesmithgh/kitty-scrollback.nvim',
+    enabled = true,
+    lazy = true,
+    cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth', 'KittyScrollbackGenerateCommandLineEditing' },
+    event = { 'User KittyScrollbackLaunch' },
+    version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
+    config = function()
+      require('kitty-scrollback').setup({
+        {
+          visual_selection_highlight_mode = 'kitty',
+        }
+      })
+    end,
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function(_, conf)
+      conf.pickers = {
+        current_buffer_fuzzy_find = {
+          previewer = false,
+        },
+      }
+      return conf
+    end,
+  }
 }
