425a441bebde5ad43a9c4966e6273e83f8fd119f
shuymn <mail@shuymn.me>
2025-01-07 17:19:02 +0900

    feat(zsh): add godl function for installing Go versions with optional GOROOT update

--- .config/zsh/config/03_command.zsh
+++ .config/zsh/config/03_command.zsh
@@ -105,3 +105,60 @@ gomi() {
   local root && root=$(ghq root)
   go mod init $(pwd | sed -e "s#$root/##g")
 }
+
+godl() {
+  local version
+  local update_goroot=true
+
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+    latest)
+      version=$(https 'go.dev/dl/?mode=json' | jq -r '.[] | select(.stable == true) | .version' | head -n 1)
+      shift
+      ;;
+    --no-update)
+      update_goroot=false
+      shift
+      ;;
+    -h | --help)
+      echo "Usage: godl [latest|<version>] [--no-update]"
+      echo "  latest        Install the latest stable version of Go"
+      echo "  <version>     Install a specific version of Go"
+      echo "  --no-update   Install without setting GOROOT"
+      return 0
+      ;;
+    *)
+      if [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        version="go$1"
+        shift
+      else
+        echo "\"$version\" is invalid version format. Abort."
+        return 1
+      fi
+      ;;
+    esac
+  done
+
+  if [ -z "$version" ]; then
+    version=$(https 'go.dev/dl/?mode=json' | jq -r '.[] | select(.stable == true) | .version' | fzf)
+    if [ -z "$version" ]; then
+      echo "No version selected. Abort."
+      return 1
+    fi
+  fi
+
+  go install "golang.org/dl/$version@latest" && $version download
+
+  if $update_goroot; then
+    local config_file && config_file="$XDG_CONFIG_HOME/zsh/config/99_local.zsh"
+    if grep -q "export GOROOT=" "$config_file"; then
+      local temp_file && temp_file=$(mktemp)
+      local new_goroot_line='export GOROOT=$('$version' env GOROOT)'
+      awk -v new_line="$new_goroot_line" '/export GOROOT=/ {print new_line; next} {print}' $config_file >"$temp_file" &&
+        mv "$temp_file" "$config_file" &&
+        echo "Updated GOROOT"
+    else
+      echo "GOROOT is not set in ${config_file}. Skip updating GOROOT."
+    fi
+  fi
+}
