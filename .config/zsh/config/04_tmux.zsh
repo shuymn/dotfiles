5b6aa063dd4017b2f248d96d2f5e1a85539f1a7c
Shu Yamani <shu.yamani@yappli.co.jp>
2025-01-07 13:59:05 +0900

    chore(zsh): comment out tmux session management code for clarity

--- .config/zsh/config/04_tmux.zsh
+++ .config/zsh/config/04_tmux.zsh
@@ -2,27 +2,27 @@ if has "tmux" && [[ $TERM_PROGRAM == "iTerm.app" ]]; then
   export ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX=YES
 fi
 
-if has "fzf" && has "tmux"; then
-  if [ -z $TMUX ] && [[ $- == *l* ]] && [[ $TERM_PROGRAM != "vscode" ]]; then
-    local sess_id
-    sess_id="$(tmux ls 2>/dev/null)"
-    if [[ -z "$sess_id" ]]; then
-      tmux new-session
-    fi
-
-    readonly local msg="Create new session"
-    readonly local sess_msg="${sess_id}\n${msg}"
-    sess_id="$(echo $sess_msg | fzf | cut -d: -f1)"
-    if [[ "$sess_id" = "$msg" ]]; then
-      tmux new-session
-    elif [[ -n "$sess_id" ]]; then
-      tmux attach-session -t "$sess_id"
-    else
-      # start terminal normally
-      :
-    fi
-  fi
-fi
+# if has "fzf" && has "tmux"; then
+#   if [ -z $TMUX ] && [[ $- == *l* ]] && [[ $TERM_PROGRAM != "vscode" ]]; then
+#     local sess_id
+#     sess_id="$(tmux ls 2>/dev/null)"
+#     if [[ -z "$sess_id" ]]; then
+#       tmux new-session
+#     fi
+#
+#     readonly local msg="Create new session"
+#     readonly local sess_msg="${sess_id}\n${msg}"
+#     sess_id="$(echo $sess_msg | fzf | cut -d: -f1)"
+#     if [[ "$sess_id" = "$msg" ]]; then
+#       tmux new-session
+#     elif [[ -n "$sess_id" ]]; then
+#       tmux attach-session -t "$sess_id"
+#     else
+#       # start terminal normally
+#       :
+#     fi
+#   fi
+# fi
 
 # tmux package manager
 if has "tmux" && [[ ! -d "${HOME}/.tmux/plugins/tpm" ]]; then
