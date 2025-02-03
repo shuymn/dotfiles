d2786b5b09a5f763a9829376ea97a6a9dec450ba
Shu Yamani <shu.yamani@yappli.co.jp>
2025-02-03 12:23:47 +0900

    refactor(nvim): KittyScrollbackプラグインのenabledオプションを削除

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -45,7 +45,6 @@ return {
 
   {
     'mikesmithgh/kitty-scrollback.nvim',
-    enabled = true,
     lazy = true,
     cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth', 'KittyScrollbackGenerateCommandLineEditing' },
     event = { 'User KittyScrollbackLaunch' },
