887e9eeb4294e309d5c8e331c0c2c48dca5a6cb7
Shu YAMANI <mail@shuymn.me>
2026-01-14 16:45:07 +0900

    fix(init.lua): uncomment kitty-scrollback.nvim plugin configuration

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -43,20 +43,20 @@ return {
     end,
   },
 
-  --   {
-  --     'mikesmithgh/kitty-scrollback.nvim',
-  --     lazy = true,
-  --     cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth', 'KittyScrollbackGenerateCommandLineEditing' },
-  --     event = { 'User KittyScrollbackLaunch' },
-  --     version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
-  --     config = function()
-  --       require('kitty-scrollback').setup({
-  --         {
-  --           visual_selection_highlight_mode = 'kitty',
-  --         }
-  --       })
-  --     end,
-  --   },
+    {
+      'mikesmithgh/kitty-scrollback.nvim',
+      lazy = true,
+      cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth', 'KittyScrollbackGenerateCommandLineEditing' },
+      event = { 'User KittyScrollbackLaunch' },
+      version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
+      config = function()
+        require('kitty-scrollback').setup({
+          {
+            visual_selection_highlight_mode = 'kitty',
+          }
+        })
+      end,
+    },
 
   {
     "nvim-telescope/telescope.nvim",
