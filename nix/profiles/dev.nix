0424c551ca8593575db40c9c765f8afc6f068b7c
Shu YAMANI <mail@shuymn.me>
2026-06-05 04:20:53 +0900

    feat(nix): add development tooling

--- nix/profiles/dev.nix
+++ nix/profiles/dev.nix
@@ -2,17 +2,23 @@
 
 {
   home.packages = with pkgs; [
+    astro-language-server
     beads
+    biome
     ghq
     golangci-lint
     gopls
     gotools
     govulncheck
     mo
+    nixd
+    nixfmt
     pre-commit
+    prisma-language-server
     rustup
     shellcheck
     shfmt
+    vscode-langservers-extracted
     yamllint
   ];
 }
