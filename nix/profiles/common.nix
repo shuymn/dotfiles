c5802eadafea09d5470ee7f82d152167058a7db7
Shu YAMANI <mail@shuymn.me>
2026-06-05 04:20:39 +0900

    feat(nix): add neovim configuration

--- nix/profiles/common.nix
+++ nix/profiles/common.nix
@@ -38,6 +38,7 @@
     vimAlias = true;
 
     plugins = with pkgs.vimPlugins; [
+      vim-sleuth
       {
         plugin = github-nvim-theme;
         optional = true;
@@ -77,6 +78,53 @@
     ];
 
     initLua = ''
+      -- Lightweight defaults for short $EDITOR sessions.
+      vim.g.mapleader = " "
+      vim.g.maplocalleader = " "
+
+      local opt = vim.opt
+
+      opt.autoread = true
+      opt.breakindent = true
+      opt.confirm = true
+      opt.cursorline = true
+      opt.ignorecase = true
+      opt.linebreak = true
+      opt.number = true
+      opt.scrolloff = 4
+      opt.sidescrolloff = 8
+      opt.signcolumn = "yes"
+      opt.smartcase = true
+      opt.splitbelow = true
+      opt.splitright = true
+      opt.termguicolors = true
+      opt.undofile = true
+      opt.wildmode = "longest:full,full"
+      opt.wrap = true
+
+      vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { silent = true })
+      vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+      vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+      vim.keymap.set("n", "x", '"_x', { silent = true })
+      vim.keymap.set("x", "x", '"_x', { silent = true })
+
+      vim.api.nvim_create_autocmd("BufReadPost", {
+        callback = function()
+          local mark = vim.api.nvim_buf_get_mark(0, '"')
+          local line_count = vim.api.nvim_buf_line_count(0)
+
+          if mark[1] > 0 and mark[1] <= line_count then
+            pcall(vim.api.nvim_win_set_cursor, 0, mark)
+          end
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("TextYankPost", {
+        callback = function()
+          vim.highlight.on_yank({ timeout = 150 })
+        end,
+      })
+
       vim.cmd.packadd("github-nvim-theme")
       vim.cmd.packadd("nvim-treesitter")
 
