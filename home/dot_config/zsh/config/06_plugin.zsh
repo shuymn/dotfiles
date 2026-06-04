7a917580d462ab9e6cde870793a364dc6a3c2ead
Shu YAMANI <mail@shuymn.me>
2026-06-05 05:30:18 +0900

    refactor(zsh): add DOTFILES_NIX_PROFILE_DIRS array

--- home/dot_config/zsh/config/06_plugin.zsh
+++ home/dot_config/zsh/config/06_plugin.zsh
@@ -1,16 +1,13 @@
 # zsh fast syntax highlighting
-for plugin in \
-  "/etc/profiles/per-user/${DOTFILES_USER}/share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh" \
-  "${HOME}/.nix-profile/share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh" \
-  "/run/current-system/sw/share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh" \
-  "/nix/var/nix/profiles/default/share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
+for profile in "${DOTFILES_NIX_PROFILE_DIRS[@]}"
 do
+  plugin="${profile}/share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
   if [[ -f "${plugin}" ]]; then
     load "${plugin}"
     break
   fi
 done
-unset plugin
+unset profile plugin
 
 # ni.zsh
 load "$HOME/.config/zsh/plugins/ni.zsh"
