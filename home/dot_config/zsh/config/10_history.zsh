913bb49695b7115f9ac83edda9975c1969f51e8e
Shu YAMANI <mail@shuymn.me>
2026-06-09 04:56:13 +0900

    chore(zsh): switch history to session-local mode

--- home/dot_config/zsh/config/10_history.zsh
+++ home/dot_config/zsh/config/10_history.zsh
@@ -1,6 +1,8 @@
 # History
 setopt extended_history
-setopt share_history
+unsetopt share_history
+unsetopt inc_append_history
+unsetopt append_history
 setopt hist_expire_dups_first
 setopt hist_find_no_dups
 setopt hist_ignore_all_dups
