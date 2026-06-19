9c9f2d84be2cad8583bb421cae41350771a5d275
Shu YAMANI <mail@shuymn.me>
2026-06-19 12:48:32 +0900

    feat(gh): add configuration settings

--- nix/home/profiles/dev.nix
+++ nix/home/profiles/dev.nix
@@ -51,9 +51,72 @@ in
 
   programs.gh = {
     enable = true;
+
+    gitCredentialHelper.enable = false;
+
+    settings = {
+      git_protocol = "ssh";
+      prompt = "enabled";
+      pager = "hunk pager";
+    };
   };
 
   programs.gh-dash = {
     enable = true;
+
+    settings = {
+      prSections = [
+        {
+          title = "Mine";
+          filters = "is:open author:@me";
+        }
+        {
+          title = "Needs Review";
+          filters = "is:open review-requested:@me -author:@me";
+        }
+        {
+          title = "Involved";
+          filters = "is:open involves:@me -author:@me -review-requested:@me";
+        }
+        {
+          title = "Recently Merged";
+          filters = "is:merged author:@me";
+          limit = 10;
+        }
+      ];
+
+      issuesSections = [
+        {
+          title = "Assigned";
+          filters = "is:open assignee:@me";
+        }
+        {
+          title = "Created";
+          filters = "is:open author:@me";
+        }
+        {
+          title = "Involved";
+          filters = "is:open involves:@me -author:@me";
+        }
+      ];
+
+      defaults = {
+        view = "prs";
+        refetchIntervalMinutes = 10;
+        prsLimit = 20;
+        issuesLimit = 20;
+
+        preview = {
+          open = true;
+          width = 80;
+        };
+      };
+
+      pager = {
+        diff = "hunk pager";
+      };
+
+      smartFilteringAtLaunch = true;
+    };
   };
 }
